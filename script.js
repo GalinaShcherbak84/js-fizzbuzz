d0661c061e7b1ef8094d674d46ed60da76b7bbd9
GalinaSherbak84 <galinasherbak@gmail.com>
2021-03-23 17:34:31 +0100

    FizzBuzz

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //stampare i numeri da 1 a 100
-var volte = 100;
-var numeroProgress = 0
-for(i=1; i<= volte; i++){
-    console.log(i);
+var richieste = 100;
+for(var i = 1; i <= richieste; i++){
+    if((i % 3 === 0) && (i % 5 === 0)){
+      console.log("FizzBuzz");
+    } else if(i % 3 === 0){
+        console.log("Fizz");
+    } else if(i % 5 === 0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
 }
+
